961f880f195bb39ef2af6e4282d2e3de04f2dcc8
takashi <takashikami@gmail.com>
2024-07-09 23:14:21 +0900

    initial commit

--- spec/factories/manga.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :manga do
-    association :category, factory: :category
-    association :author, factory: :author
-    association :publisher, factory: :publisher
-    title { 'タイトル' }
-    description { '説明文' }
-  end
-end

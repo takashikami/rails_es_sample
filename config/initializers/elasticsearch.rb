5ae7f3f548691e82759acc55b2dc4038d2088c4c
takashi <takashikami@gmail.com>
2024-07-15 22:24:17 +0900

    OpenSearchに変更

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,11 +1,14 @@
-Elasticsearch::Model.client = Elasticsearch::Client.new(
-  host: 'localhost:9200',
-  user: 'elastic',
-  password: "elastic",
-)
+# ActiveRecord::Base.logger = Logger.new(STDOUT)
+# Rails.logger.level = Logger::DEBUG
 
-# Elasticsearch::Model.client = OpenSearch::Client.new(
+# Elasticsearch::Model.client = Elasticsearch::Client.new(
 #   host: 'localhost:9200',
-#   user: 'admin',
-#   password: "2wsxCDE#4rfv",
+#   user: 'elastic',
+#   password: "elastic",
 # )
+
+Elasticsearch::Model.client = OpenSearch::Client.new(
+  host: 'localhost:9200',
+  user: 'admin',
+  password: "2wsxCDE#4rfv",
+)
